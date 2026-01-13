537126f7a6bc48071d5aba53ffdb46534ef49792
ThiagoLinux4life <thiagostarting@gmail.com>
2026-01-13 09:50:02 -0300

    Conversão de Temperatura

--- Pratica03_exercicio3.py
+++ Pratica03_exercicio3.py
@@ -4,7 +4,11 @@
 #O usuário deve informar a temperatura, 
 # a unidade de origem e a unidade para qual deseja converter.
 
-temperatura = int(input("Entre com a Temperatura atual: "))
+temperatura = int(input("Informe a Temperatura atual: "))
+unidadedeorigem = input("Informe a unidade de origem: ")
+unidadededestino = input("Informe a unidade para qual deseja converter: ")
+
+
 
 convertcelsius_kelvin = (temperatura + 273.15)
 convertkelvin_celsius =  (temperatura - 273.15)
@@ -13,20 +17,20 @@ convertfahrenheit_celsius = (temperatura - 32) * 5/9
 convertkelvin_fahrenheit = (temperatura - 273.15) * 9/5 + 32
 convertfahrenheit_kelvin = (temperatura - 32) * 5/9 + 273.15
 
-if convertcelsius_kelvin:
+if unidadedeorigem == "C" and unidadededestino == "K":
     print(f"{convertcelsius_kelvin:.2f} °K")
 
-if convertkelvin_celsius:
+if unidadedeorigem == "K" and unidadededestino == "C": 
     print(f"{convertkelvin_celsius:.2f} ° C")
 
-if convertcelsius_fahrenheit:
+if unidadedeorigem == "C" and unidadededestino == "F":
     print(f"{convertcelsius_fahrenheit:.2f} ° F")
 
-if convertfahrenheit_celsius:
+if unidadedeorigem == "F" and unidadededestino == "C":
     print(f"{convertfahrenheit_celsius:.2f} ° C")
 
-if convertkelvin_fahrenheit:
+if unidadedeorigem == "K" and unidadededestino == "F":
     print(f"{convertkelvin_fahrenheit:.2f} ° F")
 
-if convertfahrenheit_kelvin:
+if unidadedeorigem == "F" and unidadededestino == "K":
     print(f"{convertfahrenheit_kelvin:.2f} ° K")
